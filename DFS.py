96e21ada9a31282290fe503329ef75a7598fc4af
lukas <lukasz.kordon@outlook.com>
2021-05-24 00:51:22 +0200

    depth-first-search-recursive

---
+++ DFS.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 20:58:59 2021
+
+@author: lukas
+"""
+licz_sloni = 6
+mas_slon = [2400, 2000, 1200, 2400, 1600, 4000]
+kol_startowa = [1, 4, 5, 3, 6, 2]
+kol_docelowa = [[5], [3], [2], [4], [6], [1]]
+
+masa_sloni = dict(zip(kol_startowa, mas_slon))
+graph = dict(zip(kol_startowa, kol_docelowa))
+print(graph)
+visited = set() # Set to keep track of visited nodes.
+my_cycles = []
+
+
+import itertools
+def dfs_it(graph, start_node, end_node, p):
+
+    print(p)
+    frontier = []
+    frontier.append(start_node)
+    explored = set()  
+    while frontier:
+        current_node = frontier.pop()
+        if current_node in explored: continue
+        if current_node == end_node: 
+            if end_node not in p:
+                p.append(end_node) 
+                    
+        for neighbor in graph[current_node]:
+            frontier.append(neighbor)        
+            explored.add(current_node) 
+    return p
+
+
+def aaa(j):
+    p = []
+    for i in range(1, licz_sloni + 1):
+        # print(my_cycles)
+        # for j in range(1, licz_sloni + 1):
+        cycl = dfs_it(graph, j, i, p)
+        my_cycles.append(cycl)
+        
+for i in range(1, 7):
+    aaa(i)
+
+    
+my_cycles.sort()
+my_cycles = list(k for k,_ in itertools.groupby(my_cycles))
+    
+
+
+print("\nCycles: \n", my_cycles)
--- slonie.py
+++ slonie.py
@@ -5,15 +5,35 @@ Created on Sat May 22 00:48:15 2021
 
 @author: lukas
 """
-licz_sloni = 10 # n
-mas_slon = [3015, 4728, 4802, 4361, 135, 4444, 4313, 1413, 4581, 546]
-kol_startowa = [3, 10, 1, 8, 9, 4, 2, 7, 6, 5]
-kol_docelowa = [[4], [9], [5], [3], [1], [6], [10], [7], [8], [2]]
-wynik_koncowy = 0
+import re 
+
+# lines = []  
+# with open('zadanie_B/slo1.in', 'r') as f:
+#     for line in f:
+#         line = line.strip()
+#         line = re.sub("\s+", ", ", line.strip())
+#         line =[int(x) for x in line.split(',')]
+#         lines.append(line)
+
+
+# licz_sloni = int(lines[0][0])
+# mas_slon = lines[1]
+# kol_startowa = lines[2]
+# kol_docelowa = [[x] for x in lines[3]]
+
+# print(licz_sloni)
+# print(mas_slon)
+# print(kol_startowa)
+# print(kol_docelowa)
+
+licz_sloni = 6
+mas_slon = [2400, 2000, 1200, 2400, 1600, 4000]
+kol_startowa = [1, 4, 5, 3, 6, 2]
+kol_docelowa = [[5], [3], [2], [4], [6], [1]]
 
 masa_sloni = dict(zip(kol_startowa, mas_slon))
 graph = dict(zip(kol_startowa, kol_docelowa))
-
+# print(graph)
 visited = set() # Set to keep track of visited nodes.
 my_cycles = []
 
@@ -32,11 +52,12 @@ def dfs(visited, graph, node, p):
 for i in range(1, licz_sloni + 1):  
     p = []      
     x = dfs(visited, graph, i, p)
+    # x = dfs_it(graph, i)
     my_cycles.append(x)
     # print(x)
 
 my_cycles = [x for x in my_cycles if x]
-print(my_cycles, "my_cycles\n")
+print("\nCycles: \n", my_cycles)
 
 
 # wyznaczanie param cykli
@@ -88,32 +109,67 @@ def oblicz_wyniku():
     
     #
     for i, elem in enumerate(my_cycles, start = 1):
+        print("Numer cyklu: ", i)
         cmc = calkowi_masa_cyklu[i]
         mmwc = min_masa_w_cyklu[i]
-        Cl = len(elem)
-        print(Cl, "Cl")
-        print(cmc, "calk_masa_cykl")
-        print(mmwc, "min mas w cyklu")
-        metoda1.append(cmc + (abs(Cl) - 2) * mmwc)
-        metoda2.append(cmc + mmwc + (abs(Cl) + 1) * min_)
-        # w = w + min(metoda1, metoda2)
+        dlugos_cyckl = len(elem)
+        
+        print("Dlugos_cyckl: ", dlugos_cyckl)
+        print("Calk_masa_cykl:", cmc)
+        print("Min masa w cyklu", mmwc, "\n")
+        
+        # if abs(dlugos_cyckl) >= 0:
+            
+        metoda11 = cmc + (abs(dlugos_cyckl - 1) ) * mmwc
+        metoda22 = cmc + mmwc + (abs(dlugos_cyckl + 1)) * min_
+            
+        metoda1.append(metoda11)
+        metoda2.append(metoda22)
+        
+        print("Metoda1 w cyklu: ", metoda11)
+        print("Metoda2 w cyklu: ", metoda22)
+       
+        licz1 = min(metoda1)
+        licz2 = min(metoda2)
+        licz3 = min(licz1, licz2)
+        
+        print(licz1, "licz1")        
+        print(licz2, "licz2")    
+        print(licz3, "licz3\n")
+        # w = w + licz3
     
-    print(metoda1, "metoda1")
-    print(metoda2, "metoda2")
-    metoda1 = sum(metoda1)
-    metoda2 = sum(metoda2)
+    print("Metoda1: ", metoda1)
+    print("Metoda2: ", metoda2)
+    
+    liss = []
+    for i in range(3):
+        met1 = metoda1[i]
+        met2 = metoda2[i]
+        max_ = max(met1, met2)
+        min_ = min(met1, met2)
+        w = max_ - min_
+        liss.append(w)
+    
+    
+    
+    # metoda1 = sum(metoda1)
+    # metoda2 = sum(metoda2)
+    
+    # max_ = max(metoda1, metoda2)
+    # min_ = min(metoda1, metoda2)
+    
+    # w = max_ - min_
+    
     print(metoda1)
     print(metoda2)
-    print(w)
-    print(metoda1 + metoda2)
-    print(metoda1 + metoda1)
-    print(metoda2 + metoda2)
-    print(w + w)
+    print("w:", sum(liss))
     # 30518
+    # 11200
         
 # print(sumaC)        
-wyzn_para_cykl()
-oblicz_wyniku()
+# wyzn_para_cykl()
+# oblicz_wyniku()
+
 
 
 # if __name__ == "__main__":  
